4b6b84941a66b793468d9b3c1f86798df2237016
David Avalos <davalos@itparral.edu.mx>
2022-05-18 10:51:43 -0600

    Aparecen los datos correctos de preguntas al incio

--- routes/inicioR.js
+++ routes/inicioR.js
@@ -2,12 +2,22 @@ const Joi = require("joi");
 var express = require('express');
 var router = express.Router();
 const validator = require('express-joi-validation').createValidator({});
+let pool  =  require('../db/config.js');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log(req.session.usid);
-  res.render('inicio', { title: 'Express' , sesion: req.session});
+  pool.getConnection().then(conn =>{
+    conn.query("SELECT publicaciones_aprobadas.id_publicacion, publicaciones.titulo, publicaciones.fecha, usuarios.nombre, (SELECT COUNT(*) FROM comentarios WHERE comentarios.id = publicaciones_aprobadas.id_publicacion) AS num_comentarios FROM `publicaciones_aprobadas` INNER JOIN publicaciones ON publicaciones_aprobadas.id_publicacion = publicaciones.id INNER JOIN usuarios ON usuarios.id = publicaciones.id_propietario ORDER BY publicaciones.fecha").then(r => {
+      res.render('inicio', { title: 'Express' , sesion: req.session, r:r});
+    }).catch(err => {
+      res.send("ERROR CONSULTA");
+    });
+    conn.end();
+  }).catch(err => {
+    res.send("ERROR CONECTAR DB");
+  })
 });
 
 module.exports = router;
