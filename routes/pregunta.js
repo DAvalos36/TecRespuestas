263b0af2be227b7c44a245cbc94b39a662050211
David Avalos <davalos@itparral.edu.mx>
2022-05-18 16:36:38 -0600

    Salen las publicaciones como debe ser

--- routes/pregunta.js
+++ routes/pregunta.js
@@ -1,7 +1,9 @@
 var express = require('express');
 const Joi = require('joi');
+const { query } = require('../db/config.js');
 var router = express.Router();
 const validator = require('express-joi-validation').createValidator({});
+let pool  =  require('../db/config.js');
 
 
 const esquemaId = Joi.object({
@@ -13,7 +15,26 @@ router.get('/',function(req, res, next) {
     res.redirect("/");
 });
 router.get('/:id', validator.params(esquemaId),function(req, res, next) {
-    res.render('preguntaEspecifica', {id: req.params.id, sesion: req.session});
+    pool.getConnection().then(conn => {
+        conn.query("SELECT publicaciones.id, publicaciones.titulo, publicaciones.contenido, publicaciones.fecha, CONCAT(usuarios.nombre, ' ', usuarios.apellido) AS nombre FROM publicaciones_aprobadas INNER JOIN publicaciones ON publicaciones.id = publicaciones_aprobadas.id_publicacion INNER JOIN usuarios ON usuarios.id = publicaciones.id_propietario WHERE publicaciones_aprobadas.id_publicacion = ?", [req.params.id]).then(r => {
+            if(r.length === 1){
+                conn.query("SELECT comentarios.id, comentarios.comentario, CONCAT(usuarios.nombre, ' ', usuarios.apellido) as nombre, comentarios.fecha_comentado, comentarios.id_propietario FROM comentarios INNER JOIN usuarios ON comentarios.id_propietario = usuarios.id WHERE comentarios.id_publi = ?", [req.params.id]).then(comentarios => {
+                    res.render('preguntaEspecifica', {id: req.params.id, sesion: req.session, pregunta: r[0], comentarios});
+                }).catch(err=>{
+                    console.log("IDK");
+                });
+            }
+            else{
+                // CONSTRUIR PAGINA PARA ESTO
+                res.send("<h1>Pregunta no encontrada...</h1>")
+            }
+        }).catch(err =>{
+            res.status(400).json({msj: "Ocurrio un error al realizar la consulta.", error:err});
+        });
+        conn.end();
+    }).catch(err => {
+        res.status(500).json({msj: "Base de datos no conectada", error: err});
+    });
 });
 
 
