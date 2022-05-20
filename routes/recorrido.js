64615c912e50a9939ade1819129d5c8ea237e5f2
David Avalos <davalos@itparral.edu.mx>
2022-05-20 12:28:25 -0600

    Solucion recorrido

--- routes/recorrido.js
+++ routes/recorrido.js
@@ -4,5 +4,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render("recorrido", {sesion: req.session} );
 });
+router.get('/redireccionar', function(req, res, next) {
+  res.redirect("http://www.itparral.edu.mx/images/RecorridosVirtuales/Pano2VR/output/index.html");
+});
+
 
 module.exports = router;
