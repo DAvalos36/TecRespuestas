f03614f3bbcff690a182426719668943dd4ca14b
David Avalos <davalos@itparral.edu.mx>
2022-05-21 13:48:41 -0600

    Bug corregido

--- routes/inicioR.js
+++ routes/inicioR.js
@@ -9,7 +9,7 @@ let pool  =  require('../db/config.js');
 router.get('/', function(req, res, next) {
   console.log(req.session.usid);
   pool.getConnection().then(conn =>{
-    conn.query("SELECT publicaciones_aprobadas.id_publicacion, publicaciones.titulo, publicaciones.fecha, CONCAT(usuarios.nombre, ' ', usuarios.apellido) AS nombre, (SELECT COUNT(*) FROM comentarios WHERE comentarios.id = publicaciones_aprobadas.id_publicacion) AS num_comentarios FROM `publicaciones_aprobadas` INNER JOIN publicaciones ON publicaciones_aprobadas.id_publicacion = publicaciones.id INNER JOIN usuarios ON usuarios.id = publicaciones.id_propietario ORDER BY publicaciones.fecha").then(r => {
+    conn.query("SELECT publicaciones_aprobadas.id_publicacion, publicaciones.titulo, publicaciones.fecha, CONCAT( usuarios.nombre, ' ', usuarios.apellido ) AS nombre, ( SELECT COUNT(*) FROM comentarios WHERE comentarios.id_publi = publicaciones_aprobadas.id_publicacion ) AS num_comentarios FROM `publicaciones_aprobadas` INNER JOIN publicaciones ON publicaciones_aprobadas.id_publicacion = publicaciones.id INNER JOIN usuarios ON usuarios.id = publicaciones.id_propietario ORDER BY publicaciones.fecha").then(r => {
       res.render('inicio', { title: 'Express' , sesion: req.session, r:r});
     }).catch(err => {
       res.send("ERROR CONSULTA");
